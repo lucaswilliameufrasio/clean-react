582c32b3be8222062f4c5f2f7b69e2c54de40402
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-02 15:55:45 -0300

    test: ensure MinLengthValidation returns falsy if value length is greater the specified

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,4 +8,11 @@ describe('MinLengthValidation', () => {
 
     expect(error).toEqual(new InvalidFieldError('field'))
   })
+
+  test('Should return falsy if value length is greater the specified', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+
+    expect(error).toBeFalsy()
+  })
 })

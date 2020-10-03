02fbf0b03fb6846011841a084d11769c0605492b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-03 02:49:23 -0300

    refactor: change api url factory to receive path by param

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,3 +1,3 @@
-export const makeApiUrl = (): string => {
-  return 'https://clean-ts-api-lucas.herokuapp.com/api/login'
+export const makeApiUrl = (path: string): string => {
+  return `https://clean-ts-api-lucas.herokuapp.com/api${path}`
 }

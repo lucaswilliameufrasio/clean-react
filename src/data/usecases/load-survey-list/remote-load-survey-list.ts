96ab7f1114858469d4de0730e201abbaccb683b7
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-02 06:27:10 -0300

    feat: ensure RemoteSurveyList map remote data to domain data

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -6,14 +6,13 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
   constructor (
     private readonly url: string,
     private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyList.Model[]>
-  ) {}
+  ) { }
 
   async loadAll (): Promise<LoadSurveyList.Model[]> {
-    const httpResponse = await this.httpGetClient.get({
-      url: this.url
-    })
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+    const remoteSurveys = httpResponse.body || []
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return httpResponse.body
+      case HttpStatusCode.ok: return remoteSurveys.map(remoteSurvey => ({ ...remoteSurvey, date: new Date(remoteSurvey.date) }))
       case HttpStatusCode.noContent: return []
       default: throw new UnexpectedError()
     }
@@ -21,5 +20,10 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
 }
 
 export namespace RemoteLoadSurveyList {
-  export type Model = LoadSurveyList.Model
+  export type Model = {
+    id: string
+    question: string
+    date: string
+    didAnswer: boolean
+  }
 }

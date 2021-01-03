35d6b413f786d3ef040e50bd97681fa204dd306e
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-03 09:11:47 -0300

    refactor: change SurveyResult highlight item

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -8,13 +8,23 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        {false && (
+        {true && (
           <>
             <hgroup>
               <Calendar date={new Date()} className={Styles.calendarWrap}/>
               <h2>Qual é seu framework web favorito?</h2>
             </hgroup>
             <FlipMove className={Styles.answersList}>
+              <li>
+                <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
               <li>
                 <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
                 <span className={Styles.answer}>ReactJS</span>

1804b77f505208df2402c6775d7d3350643a0bc0
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-03 18:27:55 -0300

    test: ensure RemoteSaveSurveyResult calls HttpClient with correct body

--- src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -1,6 +1,7 @@
 import { RemoteSaveSurveyResult } from '@/data/usecases'
-import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
+import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
+import { mockSaveSurveyResultParams } from '@/domain/test'
 import faker from 'faker'
 
 type SutTypes = {
@@ -17,15 +18,17 @@ const makeSut = (url = faker.internet.url()): SutTypes => {
   }
 }
 describe('RemoteSaveSurveyResult', () => {
-  test('Should call HttpClient with correct url and method', async () => {
+  test('Should call HttpClient with correct values', async () => {
     const url = faker.internet.url()
     const { sut, httpClientSpy } = makeSut(url)
     httpClientSpy.response = {
       statusCode: HttpStatusCode.ok,
       body: mockRemoteSurveyResultModel()
     }
-    await sut.save({ answer: faker.random.word() })
+    const saveSurveyResultParams = mockSaveSurveyResultParams()
+    await sut.save(saveSurveyResultParams)
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('put')
+    expect(httpClientSpy.body).toEqual(saveSurveyResultParams)
   })
 })

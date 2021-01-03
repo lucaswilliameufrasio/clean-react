e8625806ae0d4c947aebcdb0e8d37a066731d018
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-03 14:37:09 -0300

    feat: add SurveyResult composition

---
+++ src/main/factories/usecases/remote-load-survey-result-factory.ts
@@ -0,0 +1,7 @@
+import { makeApiUrl, makeAuthorizeHttpGetClientDecorator } from '@/main/factories'
+import { LoadSurveyResult } from '@/domain/usecases'
+import { RemoteLoadSurveyResult } from '@/data/usecases'
+
+export const makeRemoteLoadSurveyResult = (id: string): LoadSurveyResult => {
+  return new RemoteLoadSurveyResult(makeApiUrl(`/surveys/${id}/results`), makeAuthorizeHttpGetClientDecorator())
+}

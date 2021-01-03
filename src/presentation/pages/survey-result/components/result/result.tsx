a72edbc7e8df12917d5d8addf2812fdb040e0ae7
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-03 16:05:00 -0300

    refactor: move SurveyResultAnswer to its own Component

--- src/presentation/pages/survey-result/components/result/result.tsx
+++ src/presentation/pages/survey-result/components/result/result.tsx
@@ -3,6 +3,7 @@ import FlipMove from 'react-flip-move'
 import { useHistory } from 'react-router-dom'
 import Styles from './result-styles.scss'
 import { Calendar } from '@/presentation/components'
+import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
 import { LoadSurveyResult } from '@/domain/usecases'
 
 type Props = {
@@ -18,13 +19,9 @@ const Result: React.FC<Props> = ({ surveyResult }: Props) => {
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
       <FlipMove data-testid="answers" className={Styles.answersList}>
-        {surveyResult.answers.map(answer => (
-          <li data-testid="answer-wrap" className={answer.isCurrentAccountAnswer ? Styles.active : ''} key={answer.answer}>
-            {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
-            <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
-            <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
-          </li>
-        ))}
+        <>
+          {surveyResult.answers.map(answer => <SurveyResultAnswer key={answer.answer} answer={answer} />)}
+        </>
       </FlipMove>
       <button className={Styles.button} data-testid="back-button" onClick={goBack}>Voltar</button>
     </>

24eef8518d076f25e54846eb1afb511d57c2858a
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-03 09:09:30 -0300

    feat: add skeleton shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -8,29 +8,23 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap}/>
-          <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answersList}>
-          <li>
-            <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
-        {false && <Loading />}
+        {false && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap}/>
+              <h2>Qual é seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img src="https://clean-ts-api-lucas.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+            {false && <Loading />}
+          </>
+        )}
       </div>
       <Footer />
     </div>

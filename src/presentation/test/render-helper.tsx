6b64064d76d14acce49c9a08ee35f1ad6f5a1968
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-04 01:46:54 -0300

    fix: change test to prevent multiple clicks

--- src/presentation/test/render-helper.tsx
+++ src/presentation/test/render-helper.tsx
@@ -2,30 +2,34 @@ import { AccountModel } from '@/domain/models'
 import { mockAccountModel } from '@/domain/test'
 import { currentAccountState } from '@/presentation/components'
 
-import { Router } from 'react-router-dom'
-import { MemoryHistory } from 'history'
-import { render } from '@testing-library/react'
-import { RecoilRoot } from 'recoil'
 import React from 'react'
+import { MemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
+import { render } from '@testing-library/react'
+import { MutableSnapshot, RecoilRoot, RecoilState } from 'recoil'
 
 type Params = {
   Page: React.FC
   history: MemoryHistory
   account?: AccountModel
+  states?: Array<{ atom: RecoilState<any>, value: any }>
 }
 
 type Result = {
   setCurrentAccountMock: (account: AccountModel) => void
 }
 
-export const renderWithHistory = ({ Page, history, account = mockAccountModel() }: Params): Result => {
+export const renderWithHistory = ({ Page, history, account = mockAccountModel(), states = [] }: Params): Result => {
   const setCurrentAccountMock = jest.fn()
   const mockedState = {
     setCurrentAccount: setCurrentAccountMock,
     getCurrentAccount: () => account
   }
+  const initializeState = ({ set }: MutableSnapshot): void => {
+    [...states, { atom: currentAccountState, value: mockedState }].forEach(state => set(state.atom, state.value))
+  }
   render(
-    <RecoilRoot initializeState={({ set }) => set(currentAccountState, mockedState)}>
+    <RecoilRoot initializeState={initializeState}>
       <Router history={history}>
         <Page />
       </Router>
